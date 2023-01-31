be4b76f43514f62ebd9e8f80f3b3d219197f4945
JuliansRecent <juliansrecent@gmail.com>
2023-01-30 21:05:03 -0800

    Module complete. Create a terminal based application that prints welcome is the users string input is correct.

--- src/main/java/Password.java
+++ src/main/java/Password.java
@@ -6,6 +6,18 @@ public class Password {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
+        // Write your program here
+
+        // Prompt the user for the password
+        System.out.println("Password?");
+        String password = scan.nextLine();
+
+        if (password.equals("Caput Draconis")) {
+            // Print 'Welcome!' if the password is correct
+            System.out.println("Welcome!");
+        } else {
+            // Print 'Off with you!' if the password is incorrect
+            System.out.println("Off with you!");
+        }
     }
 }
